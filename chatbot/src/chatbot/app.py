be151249afb95c871e9b1ec5074a04d50edf98f7
Subhashis Pradhan <pradhan.subhasis174@gmail.com>
2025-06-27 17:44:10 +0530

    chore: add postgres persistence for chatbot

--- chatbot/src/chatbot/app.py
+++ chatbot/src/chatbot/app.py
@@ -1,7 +1,10 @@
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, AIMessage, trim_messages
+from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver
+from psycopg_pool import AsyncConnectionPool
+import asyncio
 
 # Define a new graph
 workflow = StateGraph(state_schema=MessagesState)
@@ -15,27 +18,36 @@ def call_model(state:MessagesState):
 workflow.add_edge(START, "model")
 workflow.add_node("model",call_model)
 
-def main():
-    memory = MemorySaver()
-    app = workflow.compile(checkpointer=memory)
-    config = {"configurable": {"thread_id": "abc123"}} # the thread_id is used for memnory purposes
-    
-    query = input("> ")
-    while(query != "bye"):
-        input_messages = [HumanMessage(query)]
-        #output = app.invoke({"messages": input_messages}, config)
-        #output["messages"][-1].pretty_print()  # output contains all messages in state
+async def main():
+    async with AsyncConnectionPool(
+        conninfo=f"postgres://llm:llm@localhost:5432/llm",
+        max_size=20,
+        kwargs={
+            "autocommit":True
+        }
+    ) as pool:
+        async with pool.connection() as conn:
+            memory = AsyncPostgresSaver(conn=conn)
+            await memory.setup()
         
-        # the below is used for streaming the response
-        for chunk, metadata in app.stream(
-            {"messages":input_messages, "language":"English"},
-            config=config,
-            stream_mode="messages"):
-            if isinstance(chunk, AIMessage):
-                print(chunk.content, end="")
-        print("\n")
-        query = input("> ")
+        async with pool.connection() as conn:
+            memory = AsyncPostgresSaver(conn=conn)
+            app = workflow.compile(checkpointer=memory)
+            config = {"configurable": {"thread_id": "abc123"}} # the thread_id is used for memnory purposes
+            query = input("> ")
+            while(query != "bye"):
+                input_messages = [HumanMessage(query)]
+        
+                # the below is used for streaming the response
+                async for chunk, metadata in app.astream(
+                    {"messages":input_messages, "language":"English"},
+                    config=config,
+                    stream_mode="messages"):
+                    if isinstance(chunk, AIMessage):
+                        print(chunk.content, end="")
+                print("\n")
+                query = input("> ")
     
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
